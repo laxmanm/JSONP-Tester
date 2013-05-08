962e8061fa2b5676626faac1c6414e46c601cd29
James Singh <james@icloudtech.com>
2013-05-08 04:55:43 +0000

    Adding client side html that calls server for json response and displays it

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var fs = require("fs");
 var server = express();
 //server.enable("jsonp callback");
 server.set("jsonp callback", true);
-server.get("/", function(req, res) {
+server.get("/", function(req, res){
+	res.sendfile(__dirname+'/index.html');
+});
+server.get("/jsonp", function(req, res) {
 	fs.readFile( __dirname + '/test.json', function (err, data) {
 		if (err) {
 			throw err; 
